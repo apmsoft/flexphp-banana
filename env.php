a932e3284578cc9ce541975fe881338384217452
fancyupsoft <apmsoft@gmail.com>
2024-10-14 14:07:50 +0900

    env autoload test

--- env.php
+++ env.php
@@ -1,17 +1,13 @@
 <?php
-use Flex\Banana\Classes\App;
-use Flex\Banana\Classes\R;
-
-App::init();
-
-# 기본 Validation Resource
-R::init(App::$language ?? '');
-
 $reflector = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
 $vendorDir = realpath(dirname(dirname($reflector->getFileName())));
 $file_sysmsg = $vendorDir . '/apmsoft/flexphp-banana/res/sysmsg.json';
 if (file_exists($file_sysmsg)) {
-    R::parser($file_sysmsg, 'sysmsg');
+    Flex\Banana\Classes\App::init();
+
+    # 기본 Validation Resource
+    Flex\Banana\Classes\R::init(Flex\Banana\Classes\App::$language ?? '');
+    Flex\Banana\Classes\R::parser($file_sysmsg, 'sysmsg');
 } else {
     throw new Exception("ERROR :: sysmsg.json file not found at: " . $file_sysmsg);
 }

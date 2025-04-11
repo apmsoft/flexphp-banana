32c89c388ae35913ee14ef56ee2b3d0ca493d8b6
JongKwan <apmsoft@gamil.com>
2025-04-11 15:09:51 +0900

    - TaskFlow 에서 변수로 담거나 가지고 오거나 있거나 삭제 및 전체 데이터 가공을 편하게 할 수 있도록 업데이트

--- classes/TaskFlow.php
+++ classes/TaskFlow.php
@@ -5,9 +5,10 @@ use Flex\Banana\Classes\Array\ArrayHelper;
 
 final class TaskFlow extends ArrayHelper
 {
-    public const __version = '0.9.0';
+    public const __version = '1.0.0';
     private mixed $active = null;
     private ?callable $errorCallback = null;
+    private array $data = [];
 
     public function __construct() {}
 
@@ -27,6 +28,33 @@ final class TaskFlow extends ArrayHelper
 
         $this->active = $instance;
         return $instance;
+    }
+
+    public function set(string $key, mixed $value): static
+    {
+        $this->data[$key] = $value;
+        return $this;
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->data[$key] ?? $default;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function remove(string $key): static
+    {
+        unset($this->data[$key]);
+        return $this;
+    }
+
+    public function getAll(): array
+    {
+        return $this->data;
     }
 
     public function onError(callable $callback): static

0659d715165d383737e646540e785cff93722b2e
JongKwan <apmsoft@gmail.com>
2025-04-17 11:37:50 +0900

    d

--- interfaces/FidProviderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Flex\Banana\Interfaces;
-
-interface FidProviderInterface
-{
-    public function getTable(?string $table): string;
-    public function getFidColumnName(?string $columnName): string;
-}

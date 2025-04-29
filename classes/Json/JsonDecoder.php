ac9135dc75890108b26650c61962671d3fb05de9
JongKwan <apmsoft@gamil.com>
2025-04-29 11:44:26 +0900

    - JsonDecoder 클래스 추가 장점 : 여러 \\\로 되어 있거나 중첩 같은 것을 json decode 할 수 있도록 기능 추가

--- classes/Json/JsonDecoder.php
+++ classes/Json/JsonDecoder.php
@@ -6,7 +6,7 @@ final class JsonDecoder
     public const __version = '0.1.0';
 
     # 문자열 또는 중첩 문자열을 json 배열로 변환
-    final public static function toArray(string $value, bool $assoc = true, int $depth = 512, bool $throwExceptionOnError = false) : array
+    final public static function toArray( string $value, bool $assoc = true, int $depth = 512, bool $throwExceptionOnError = false) : array
     {
         do {
             $decoded = json_decode($value, $assoc, $depth);

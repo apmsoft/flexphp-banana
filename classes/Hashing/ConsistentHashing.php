031c94760485dac34646a5d281b0a8f3f8f96e1f
JongKwan <apmsoft@gamil.com>
2025-06-12 11:07:02 +0900

    ㅇ

--- classes/Hashing/ConsistentHashing.php
+++ classes/Hashing/ConsistentHashing.php
@@ -4,29 +4,34 @@ namespace Flex\Banana\Classes\Hashing;
 final class ConsistentHashing {
     private static array $ring = [];
     private static int $replicas = 3;
-    private static bool $initialized = false;
+    private static bool $sorted = false;
 
-    public static function init(array $servers, int $replicas = 3): void {
+    public static function setReplicas(int $replicas): void {
         self::$replicas = $replicas;
-        self::$ring = [];
-        foreach ($servers as $server) {
-            for ($i = 0; $i < $replicas; $i++) {
-                $hash = self::hash($server . ':' . $i);
-                self::$ring[$hash] = $server;
-            }
-        }
-        ksort(self::$ring);
-        self::$initialized = true;
     }
 
-    private static function hash(string $key): int {
-        return hexdec(substr(md5($key), 0, 8));
+    public static function addServer(string $server): void {
+        for ($i = 0; $i < self::$replicas; $i++) {
+            $hash = self::hash($server . ':' . $i);
+            self::$ring[$hash] = $server;
+        }
+        self::$sorted = false; // 정렬 미루기
+    }
+
+    public static function removeServer(string $server): void {
+        for ($i = 0; $i < self::$replicas; $i++) {
+            $hash = self::hash($server . ':' . $i);
+            unset(self::$ring[$hash]);
+        }
+        self::$sorted = false;
     }
 
     public static function getServer(string $key): ?string {
-        if (!self::$initialized) {
-            throw new \RuntimeException("ConsistentHashing not initialized.");
+        if (!self::$sorted) {
+            ksort(self::$ring);
+            self::$sorted = true;
         }
+
         $hash = self::hash($key);
         foreach (self::$ring as $ringHash => $server) {
             if ($hash <= $ringHash) {
@@ -34,5 +39,9 @@ final class ConsistentHashing {
             }
         }
         return reset(self::$ring);
+    }
+
+    private static function hash(string $key): int {
+        return hexdec(substr(md5($key), 0, 8));
     }
 }
